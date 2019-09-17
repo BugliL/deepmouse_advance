5431eb178f9930bc00b9249be84e9b565f936772
Bugli Lorenzo <bugli.lorenzo@gmail.com>
2019-09-17 11:45:34 +0200

    auto fitting bar

--- packages/mouse_recorder/mouse_recorder/terminal/write.py
+++ packages/mouse_recorder/mouse_recorder/terminal/write.py
@@ -1,45 +1,50 @@
 import os
 
 def move_up(times=1):
-    for _ in range(times):
-        print('',end='\x1b[A')
-
-
-def move_down(times=1):
-    for _ in range(times):
-        print('',end='\x1b[B')
+    # TODO: include me in progress_bar function, i'm too small
+    return '\x1b[A' * times
 
 
 def clear_line(end='\r'):
+    # TODO: include me in progress_bar_function, i'm too small
     characters = os.get_terminal_size().columns
-    print(' '*characters, end=end)
-
-
-def clear_lines(lines):
-    for _ in range(lines):
-        clear_line(end='\r')
-        move_up()
+    return ' '*characters + end
 
 
 def clear_all():
     lines = os.get_terminal_size().lines
-    move_down()
-    clear_lines(lines)
+    return "".join([ clear_line(end='\n') for _ in range(lines) ]) \
+        + move_up(lines)
 
 
 def progress_bar(current_value, max_value, title='', leave_title=False):
-    """
-    """
-    progress = ((current_value + 1) / max_value) * 100
-    if progress > 98:
-        progress = 100
-    clear_line()
-    bar = '[{0}{1}] {2:.1f}%'.format('#'*int(progress/2), ' ' * (50-int(progress/2)), progress)
-    output_string = ((title + '\n') if not title == '' else '')  + bar
-    print(output_string, end='')
-    if not title == '': move_up()
-    print('', end='\r')
-    if leave_title:
-        print(title)
+    percent_space = 5
+    columns = os.get_terminal_size().columns 
+    characters = columns - (4+percent_space)  # removing  present characters from counting
+    progress = min(((current_value + 1) / max_value) * 100, 100)  # calculate progress
+    printed_progress = int(progress*characters/100)  # calculate number of characters to print
+    bar_template = '\n[{:<' + str(characters) + '}] {:>'+ str(percent_space) + '.1f}%'  # creating template to print
+    
+    result = clear_line() + title  # using a variable instead of printing all time (print costs)
+    bar = bar_template.format('#'*printed_progress, progress)  # creating bar
+    result += bar + move_up() +'\r' 
+    
+    return result
+
+def print_progress_bar(current_value, max_value, title='', leave_title=False):
+    print(
+        progress_bar(current_value, max_value, title, leave_title), 
+        end=''
+    )
 
 
+if __name__=='__main__':
+    import time
+
+    for i in range(100):
+        time.sleep(0.1)
+        print_progress_bar(
+            current_value=i+80,
+            max_value=100,
+            title='Titolo di prova',
+        )
